ce0201a3881be7b7712b4926f398df2bd0b563c8
solidfox <daniel.schlaug@gmail.com>
2013-10-29 11:30:39 +0800

    Made a good test

--- spider/relationaldb_test.go
+++ spider/relationaldb_test.go
@@ -2,13 +2,25 @@ package spider
 
 import (
 	"gopher/spider"
+	"os"
 	"testing"
 )
 
+const (
+	testdbfilename = "testsqlite.db"
+)
+
 func TestInsertPages(t *testing.T) {
-	// pages := []*spider.Page{spider.NewPage()}
-	rdb := spider.NewRelationalDB("sqlite.db")
-	rdb.InsertPagesAndSetIDs(spider.Get30Pages())
-	rdb.Clear()
-	t.Fail()
+	pages := []*spider.Page{spider.NewPage(), spider.NewPage()}
+	pages[0].URL = "http://www.apple.com/"
+	pages[1].URL = "http://www.google.com/"
+	rdb := spider.NewRelationalDB(testdbfilename)
+	rdb.InsertPagesAndSetIDs(pages)
+	for i, page := range pages {
+		if page.PageID != int64(i+1) {
+			t.Log(page.PageID)
+			t.Fail()
+		}
+	}
+	os.Remove(testdbfilename)
 }
