1b4885f2c52418d3f9fba9df09f06309438f237a
solidfox <daniel.schlaug@gmail.com>
2013-11-24 14:46:43 +0800

    Ranker now works and responds with a dummy response

--- ranker/ranker.go
+++ ranker/ranker.go
@@ -1,13 +1,34 @@
 package ranker
 
+import (
+	"gopher/spider"
+	"time"
+)
+
 type Ranker struct {
 	option int
+}
+
+type Link struct {
+	title string
+	url   string
+}
+
+type ResultPage struct {
+	Title            string
+	Url              string
+	Description      string
+	Score            float64
+	ModificationDate time.Time
+	Size             int
+	Parents          []Link
+	Children         []Link
 }
 
 func NewRanker(option int) *Ranker {
 	return &Ranker{option}
 }
 
-func (r *Ranker) Search(query *Page) []*ResultPage {
-
+func (r *Ranker) Search(query *spider.Page) []*ResultPage {
+	return []*ResultPage{&ResultPage{Title: "goj"}}
 }
